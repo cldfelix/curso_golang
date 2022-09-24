ccd2461f8fe4603b37e2a8b54c67fba02502b09c
Claudinei Felix <claudinei.felix@outlook.com>
2022-09-24 15:19:11 -0300

    tentando criar hot reload, sem sucesso

--- main.go
+++ main.go
@@ -1,18 +1,25 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-
-func handleFunc(w http.ResponseWriter, r *http.Request)  {
+func handleFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Ola wwww</h1>")
 }
 
-
-func main()  {
+func main() {
+	//mux := http.NewServeMux()
+	time.Sleep(1 * time.Second)
 	http.HandleFunc("/", handleFunc)
 	fmt.Println("Iniciando server na porta :3000")
-	http.ListenAndServe(":3000", nil)
+	//fmt.Fprintln(os.Stdout, "Iniciando server na porta :3000")
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
 }
